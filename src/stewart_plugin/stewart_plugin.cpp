e0acda8bf3102fc87443723687add02ce1c5a195
Jonathan Gustafsson Frennert <jonathan.frennert@gmail.com>
2021-11-28 12:02:19 -0800

    added massless model

--- src/stewart_plugin/stewart_plugin.cpp
+++ src/stewart_plugin/stewart_plugin.cpp
@@ -87,45 +87,18 @@ namespace gazebo
             }
             std::cout << "ROS is initialized" << "\n";
 
-            /*
-            std_msgs::Float32MultiArray msg;
-
-            for (int i = 0; i < 6; i++)
-            {
-                msg.data.push_back(0);
-            }
-
-            Eigen::Matrix<float, 6, 1> x = ik(msg);
-
-            printf("address of %f\n", x[0]);
-            */
-
             // Create our ROS node. This acts in a similar manner to
             // the Gazebo node
             this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
 
-
-            /*
-            ros::SubscribeOptions so =
-              ros::SubscribeOptions::create<std_msgs::Float32>(
-                "/" + this->model->GetName() + "/vel_cmd",
-                 1,
-                 boost::bind(&StewartPlugin::OnRosMsg, this, _1),
-                 ros::VoidPtr(), &this->rosQueue);
-            this->rosSub = this->rosNode->subscribe(so);
-            */
-
-
-            std::cout << "Fuck you one!";
             // Create a named topic, and subscribe to it.
             ros::SubscribeOptions so = ros::SubscribeOptions::create<std_msgs::Float32MultiArray>(
                 "/" + this->model->GetName() + "/position_cmd",
-                1,
+                100,
                 boost::bind(&StewartPlugin::setPosition, this, _1),
                 ros::VoidPtr(), &this->rosQueue);
             this->rosSub = this->rosNode->subscribe(so);
 
-          std::cout << "Fuck you two!";
            // Spin up the queue helper thread.
            this->rosQueueThread =
              std::thread(std::bind(&StewartPlugin::QueueThread, this));
@@ -153,30 +126,6 @@ namespace gazebo
             this->rosQueue.callAvailable(ros::WallDuration(timeout));
           }
         }
-
-
-        /// \brief Handle an incoming message from ROS
-        /// \param[in] _msg A float value that is used to set the velocity
-        /// of the joints.
-        /*
-        public: void OnRosMsg(const std_msgs::Float32ConstPtr &_msg)
-        {
-          this->SetVelocity(_msg->data);
-        }
-        */
-
-
-        /// \brief Set the velocity of all the joints
-        /// \param[in] _vel New target velocity
-        /*
-        public: void SetVelocity(const double &_vel)
-        {
-          // Set the joint's target velocity.
-          for (const auto& joint: this->joints)
-            this->model->GetJointController()->SetVelocityTarget(
-                joint->GetScopedName(), _vel);
-        }
-        */
 
 
         // ----------- MODEL -----------
