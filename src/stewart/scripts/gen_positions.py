9855d7007a42c09e8061921e24c541ef7822b86b
jiiide <iJ06r#rPFh>
2021-12-01 10:18:43 -0800

    Created initial position generator for balls

---
+++ src/stewart/scripts/gen_positions.py
@@ -0,0 +1,24 @@
+import numpy as np
+import random
+
+def gen_positions(h_max, d_min, d_max, num_balls):
+    """
+    Initial Position Generator 
+    Input: Max Height, Minimum distance (float), Maximum distance (float), Number of Balls (int)
+    Output: Position of balls
+
+    """
+    result = [];
+
+    for i in range(num_balls):
+        theta = 2* np.pi * random.random() 
+        distance = d_min + (d_max - d_min) * random.random()
+
+        x = distance * np.cos(theta)
+        y = distance * np.sin(theta)
+        z = random.random() * h_max;  
+        position = np.array([x, y, z]).reshape(3,1)
+        
+        result.append(position)
+    
+    return tuple(result)
